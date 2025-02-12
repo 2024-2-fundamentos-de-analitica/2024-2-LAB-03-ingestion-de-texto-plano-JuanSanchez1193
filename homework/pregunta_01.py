df3f6d9b8963b05ade660b3536f17ac048ac1800
Juan Jose Pineda Sanchez <jpinedasa@unal.edu.co>
2025-02-12 12:16:10 -0500

    commit
    commit

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,9 +1,5 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta.
-"""
-
-# pylint: disable=import-outside-toplevel
-
+import pandas as pd
+import re
 
 def pregunta_01():
     """
@@ -18,3 +14,37 @@ def pregunta_01():
 
 
     """
+    file_path = 'files/input/clusters_report.txt'
+    
+    with open(file_path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    data = []
+    current_cluster = None
+    
+    for line in lines[4:]:  
+        line = line.rstrip()
+        
+        if not line:
+            continue  
+        
+        match = re.match(r'\s*(\d+)\s+(\d+)\s+([\d,]+)\s*%', line)
+        if match:
+            cluster = int(match.group(1))
+            cantidad_palabras = int(match.group(2))
+            porcentaje = float(match.group(3).replace(',', '.'))
+            palabras_clave = line[match.end():].strip()
+            current_cluster = [cluster, cantidad_palabras, porcentaje, palabras_clave]
+            data.append(current_cluster)
+        elif current_cluster:
+            current_cluster[3] += ' ' + line.strip()
+
+    for i in range(len(data)):
+        data[i][3] = re.sub(r'\s+', ' ', data[i][3].replace(',', ', ')).strip()
+        if data[i][3].endswith('.'):
+            data[i][3] = data[i][3][:-1]  
+    df = pd.DataFrame(data, columns=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+
+    return df
+
+print(pregunta_01())
+
